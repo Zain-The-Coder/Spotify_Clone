89fe2fc5ca84fa3826db201f217f7ba5c6ece624
Zain-The-Coder <zain015976@gmail.com>
2026-06-27 12:26:50 +0500

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const { default: chalk } = require('chalk');
 const app = require('./src/app.js');
 require('dotenv').config();
-const connectDb = require('./src/db/db.js')
+const connectDb = require('./src/db/db.js');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 connectDb();
-
